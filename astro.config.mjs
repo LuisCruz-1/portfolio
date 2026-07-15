052251f9a74d9dd8580eea064ece8e0d84cf767d
LuisCruz-1 <luiscruzm.ec@gmail.com>
2026-07-15 13:15:36 -0500

    coincidiendo aspecto con el de micrudev.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luiscruz-1.github.io',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
